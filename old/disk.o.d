af3d9246576f15a1193abc5661c55239f92a597c
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2021-05-05 19:35:52 +0000

    delete file

--- old/disk.o.d
+++
@@ -1,4 +0,0 @@
-disk.o: disk.c /usr/lib/clang/11.1.0/include/stdbool.h disk.h \
-  /usr/lib/clang/11.1.0/include/stdint.h /usr/include/stdint.h \
-  /usr/lib/clang/11.1.0/include/stddef.h \
-  /usr/lib/clang/11.1.0/include/__stddef_max_align_t.h dev/vga.h
